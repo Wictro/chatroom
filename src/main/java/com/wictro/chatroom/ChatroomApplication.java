e8168560ea8044fb6b0211de70383a0da3e042a5
Viktor Ahmeti <viktorahmeti2001@gmail.com>
2021-12-12 13:40:49 +0100

    Project Initialization

---
+++ src/main/java/com/wictro/chatroom/ChatroomApplication.java
@@ -0,0 +1,14 @@
+package com.wictro.chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class ChatroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomApplication.class, args);
+	}
+
+}
